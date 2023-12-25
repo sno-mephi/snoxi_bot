1a73b888687d7424c248e8edb0ccd31b12e4a7b6
Ivan <idfedorov09@gmail.com>
2023-12-25 19:33:53 +0300

    some refactoring

--- src/main/java/ru/idfedorov09/telegram/bot/repo/ReleaseHistoryRepository.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/repo/ReleaseHistoryRepository.kt
@@ -3,4 +3,6 @@ package ru.idfedorov09.telegram.bot.repo
 import org.springframework.data.jpa.repository.JpaRepository
 import ru.idfedorov09.telegram.bot.data.model.ReleaseHistory
 
-interface ReleaseHistoryRepository : JpaRepository<ReleaseHistory, Long>
+interface ReleaseHistoryRepository : JpaRepository<ReleaseHistory, Long> {
+    fun findFirstByOrderByIdDesc(): ReleaseHistory?
+}
